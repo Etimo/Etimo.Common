5fb748b0d15bb37e016b987d0ac17588f23edbd4
Joakim Olesen <joakim.olesen@etimo.se>
2014-06-30 00:03:42 +0200

    Modified metadata

--- Src/Etimo.Common.Collections/Properties/AssemblyInfo.cs
+++ Src/Etimo.Common.Collections/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Etimo.Common.Collections")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Common classes published by Etimo")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Etimo")]
 [assembly: AssemblyProduct("Etimo.Common.Collections")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
